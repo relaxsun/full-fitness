482e55ebf47a852d7c1eb8a3166c3f8aacc87068
relaxsun <relaxation725@gmail.com>
2024-04-13 18:46:06 +0900

    viteサーバーを起動できるようにする

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,10 +3,13 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["src/resources/css/app.css", "src/resources/js/app.js"],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ["/resources/css/app.css", "/resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    optimizeDeps: {
+        entries: [],
+    },
 });
